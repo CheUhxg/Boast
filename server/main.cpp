b31c8434d768299aae9c04a1dea09f0c86082f97
CheUhxg <3193079152@qq.com>
2022-12-31 20:58:07 +0800

    update

--- server/main.cpp
+++ server/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-void server(const char* ip, const char* port) {
+void server(const char* port) {
 
 }
 
 int main(int argc, char* argv[]) {
-    if(argc != 3) {
-        std::cout << "Usage: <prog> <ip> <port>" << std::endl;
+    if(argc != 2) {
+        std::cout << "Usage: <prog> <port>" << std::endl;
         return 1;
     }
-    server(argv[1], argv[2]);
+    server(argv[1]);
     return 0;
 }
